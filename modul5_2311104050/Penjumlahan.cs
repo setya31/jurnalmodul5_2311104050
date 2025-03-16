e8253fe110631beccb10d5d7c3fc3476f4664758
setya31 <79081950+setya31@users.noreply.github.com>
2025-03-16 22:26:30 +0700

    Menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ modul5_2311104050/Penjumlahan.cs
@@ -0,0 +1,8 @@
+﻿public class Penjumlahan
+{
+    public static T JumlahTigaAngka<T>(T a, T b, T c) where T : struct
+    {
+        dynamic x = a, y = b, z = c;
+        return x + y + z;
+    }
+}
--- modul5_2311104050/Program.cs
+++ modul5_2311104050/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Penjumlahan.JumlahTigaAngka(10, 40, 50));
+    }
+}
