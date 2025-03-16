2eaedd0f1b24c22ff9c6b4c57eea4ed7dbe90f3a
setya31 <79081950+setya31@users.noreply.github.com>
2025-03-16 22:37:04 +0700

    Menambahkan class SimpleDataBase

--- modul5_2311104050/Program.cs
+++ modul5_2311104050/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        SimpleDataBase<int> db = new SimpleDataBase<int>();
+        db.AddNewData(10);
+        db.AddNewData(40);
+        db.AddNewData(50);
+        db.PrintAllData();
+    }
+}
---
+++ modul5_2311104050/SimpleDataBase.cs
@@ -0,0 +1,19 @@
+﻿public class SimpleDataBase<T>
+{
+    private List<T> storedData = new List<T>();
+    private List<DateTime> inputDates = new List<DateTime>();
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.UtcNow);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
+        }
+    }
+}
